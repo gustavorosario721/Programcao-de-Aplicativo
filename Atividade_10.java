8235fdb7efa7b68406c68efc76be178da18df9f6
Gustavo do Rosario <kmars67pqp@gmail.com>
2026-03-01 13:42:25 +0000

    Arrumando a bagunca

--- Atividade_10.java
+++
@@ -1,9 +0,0 @@
-public class Atividade_10
-{
-    public static void main(String[] args)
-    {
-        boolean valor = false;
-
-        System.out.println("\nO valor da variável boolean é: " + valor);
-    }   
-}
